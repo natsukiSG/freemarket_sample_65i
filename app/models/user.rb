1f1f10449ee960ef3d29c04f958dc62f1af382b0
yutawanme <yarakashiwanmen@gmail.com>
2019-12-23 20:53:28 +0900

    add sns_crecdential

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,38 @@
 class User < ApplicationRecord
+  def self.from_omniauth(auth)
+    user = User.where(email: auth.info.email).first
+    sns_credential_record = SnsCredential.where(provider: auth.provider, uid: auth.uid)
+    if user.present?
+      unless sns_credential_record.present?
+        SnsCredential.create(
+          user_id: user.id,
+          provider: auth.provider,
+          uid: auth.uid
+        )
+      end
+    elsif
+      user = User.new(
+        # id: User.all.last.id + 1,
+        email: auth.info.email,
+        password: Devise.friendly_token[0, 20],
+        nickname: auth.info.name,
+        last_name: auth.info.last_name,
+        first_name: auth.info.first_name,
+      )
+      SnsCredential.new(
+        provider: auth.provider,
+        uid: auth.uid,
+        user_id: user.id
+      )
+    end 
+  user
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :sns_credentials, dependent: :destroy
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
          validates :nickname, presence: true,uniqueness: true, length: {maximum: 20}
          validates :phone_number,presence: true,uniqueness: true
