d838210048abb288ceb5a81992f58b9169dffe17
natsukiSG <natsukineria@gmail.com>
2019-12-26 12:13:58 +0900

    出品ページ実装

--- app/controllers/Toppage_controller.rb
+++ app/controllers/Toppage_controller.rb
@@ -6,4 +6,14 @@ class ToppageController < ApplicationController
 
   def show
   end
+
+  def edit
+    @product = Product.find(params[:id])
+    gon.count = @product.images.length
+    if @product.buyer_id != nil || @product.seller_id != current_user.id
+      redirect_to root_path
+    end
+    @profit = (@product.price * 0.9).round
+    @fee = @product.price - @profit
+  end
 end
