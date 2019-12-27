2c19339e3dce1e4688da6f16018479c623bbd806
yutawanme <yarakashiwanmen@gmail.com>
2019-12-27 23:36:34 +0900

    edit  model/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,8 +31,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :sns_credentials, dependent: :destroy
-  has_one :street_address, dependent: :destroy
-  accepts_nested_attributes_for :street_address
+  has_one :streetaddress, dependent: :destroy
+  accepts_nested_attributes_for :streetaddress
+
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
