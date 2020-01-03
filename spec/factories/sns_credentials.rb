b0207a438b4b1672c000e01fed8ea431bdce2ccb
yutawanme <yarakashiwanmen@gmail.com>
2020-01-03 18:24:46 +0900

    add test sns_credential

--- spec/factories/sns_credentials.rb
+++ spec/factories/sns_credentials.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :sns_credential do
-    
+    uid                          {Faker::Number.number(digits: 15)}
+    provider                     {"google"}
   end
 end
--- spec/models/sns_credential_spec.rb
+++ spec/models/sns_credential_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe SnsCredential, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe SnsCredential do
+  describe '#create' do
+
+    it "is valid with a uid, provider," do
+      snscredential = build(:sns_credential)
+      expect(snscredential).to be_valid
+    end
+    
+    it "is invalid without a uid" do
+      snscredential = build(:sns_credential, uid: "")
+      snscredential.valid?
+      expect(snscredential.errors[:uid]).to include("can't be blank")
+    end
+
+    it "is invalid without a provider" do
+      snscredential = build(:sns_credential, provider: "")
+      snscredential.valid?
+      expect(snscredential.errors[:provider]).to include("can't be blank")
+    end
+  end
 end
