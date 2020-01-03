d365809f874f48bcac338f52c2e770d0bb99d622
yutawanme <yarakashiwanmen@gmail.com>
2020-01-03 18:24:22 +0900

    add test, streetaddress

--- spec/factories/street_addresses.rb
+++ spec/factories/street_addresses.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :street_address do
-    
+    address_last_name              {"abe"}
+    address_first_name             {"fff"}
+    address_last_name_kana         {"aaa"}
+    address_first_name_kana        {"ddd"}
+    post_number                    {Faker::Number.number(digits: 7)}
+    prefectures                    {"東京都"}
+    city                           {"区"}
+    house_number                   {"00"}
   end
 end
--- spec/models/street_address_spec.rb
+++ spec/models/street_address_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
-RSpec.describe StreetAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe StreetAddress do
+  describe '#create' do
+    # let(:user){create(:user)}
+
+    it "is valid with a address_last_name, address_first_name, address_first_name_kana, address_last_name_kana, post_number, prefectures, city, house_number," do
+      streetaddress = build(:street_address)
+      expect(streetaddress).to be_valid
+    end
+    
+    it "is invalid without a address_last_name" do
+      streetaddress = build(:street_address, address_last_name: "")
+      streetaddress.valid?
+      expect(streetaddress.errors[:address_last_name]).to include("can't be blank")
+    end
+
+    it "is invalid without a address_first_name" do
+      streetaddress = build(:street_address, address_first_name: "")
+      streetaddress.valid?
+      expect(streetaddress.errors[:address_first_name]).to include("can't be blank")
+    end
+
+    it "is invalid without a address_last_name_kana" do
+      streetaddress = build(:street_address, address_last_name_kana: "")
+      streetaddress.valid?
+      expect(streetaddress.errors[:address_last_name_kana]).to include("can't be blank")
+    end
+
+    it "is invalid without a address_first_name_kana" do
+      streetaddress = build(:street_address, address_first_name_kana: "")
+      streetaddress.valid?
+      expect(streetaddress.errors[:address_first_name_kana]).to include("can't be blank")
+    end
+
+    it "is invalid without a post_number" do
+      streetaddress = build(:street_address, post_number: "")
+      streetaddress.valid?
+      expect(streetaddress.errors[:post_number]).to include("can't be blank")
+    end
+
+    it "is invalid without a prefectures" do
+      streetaddress = build(:street_address, prefectures: "")
+      streetaddress.valid?
+      expect(streetaddress.errors[:prefectures]).to include("can't be blank")
+    end
+
+    it "is invalid without a city" do
+      streetaddress = build(:street_address, city: "")
+      streetaddress.valid?
+      expect(streetaddress.errors[:city]).to include("can't be blank")
+    end
+
+    it "is invalid without a house_number" do
+      streetaddress = build(:street_address, house_number: "")
+      streetaddress.valid?
+      expect(streetaddress.errors[:house_number]).to include("can't be blank")
+    end
+  end
 end
