1a993ce6e68ea4eea2546e5900d2241e69ec7753
yutawanme <yarakashiwanmen@gmail.com>
2020-01-03 17:16:59 +0900

    edit リファクタリング

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,28 +1,23 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?  #もし@userがDBに既にいたら、ログイン状態にします  
-      sign_in_and_redirect @user, event: :authentication 
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    else  #もし@userがDBにいない場合、新規登録ページにリダイレクトします
-      session["devise.provider_data"] = request.env["omniauth.auth"]
-      #データをsessionに入れることによって、新規登録ページの入力欄に、予め情報を入れておくなどが可能になります。
-      session[:password] = SecureRandom.alphanumeric(30)
-      # binding.pry
-      redirect_to profile_path
-    end
+    authorization(:facebook)
   end
 
   def google_oauth2
+    authorization(:google)
+  end
+
+  private
+  def authorization(provider)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
+    if @user.persisted?  #もし@userがDBに既にいたら、ログイン状態にします  
       sign_in_and_redirect @user, event: :authentication 
-      set_flash_message(:notice, :success, kind: 'google') if is_navigational_format?
-    else
-      session["devise.provider_data"] = request.env["omniauth.auth"][:info]
-      #google認証の場合は、なぜかauth_hashの容量が大きく、一瞬で容量オーバーとなるため、新規登録時に必要な情報のみをsessionに渡すこととしました。（おそらく画像データのせい？）
+      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+    else  #もし@userがDBにいない場合、新規登録ページにリダイレクトします
+      session["devise.provider_data"] = request.env["omniauth.auth"].except(:extra)
+      #データをsessionに入れることによって、新規登録ページの入力欄に、予め情報を入れておくなどが可能になります。
+      session[:password] = SecureRandom.alphanumeric(30)
       redirect_to profile_path
     end
   end
 end
-# frozen_string_literal: true
