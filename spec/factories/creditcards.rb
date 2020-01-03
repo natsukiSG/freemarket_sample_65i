cc989890985b19b7db65af80c472c664cbc82354
yutawanme <yarakashiwanmen@gmail.com>
2020-01-03 18:25:07 +0900

    add test, creditcard

--- spec/factories/creditcards.rb
+++ spec/factories/creditcards.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :creditcard do
-    
+    card_id                          {Faker::Number.number(digits: 30)}
+    customer_id                      {Faker::Number.number(digits: 30)}
   end
 end
