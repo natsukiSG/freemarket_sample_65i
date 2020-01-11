36a0bee8d75597546a54d21b34c60401a51a7b55
yutawanme <yarakashiwanmen@gmail.com>
2020-01-11 11:38:03 +0900

    add chanel_brands

--- app/controllers/Toppage_controller.rb
+++ app/controllers/Toppage_controller.rb
@@ -4,6 +4,7 @@ class ToppageController < ApplicationController
     @mens_items = Product.where(category_id: 199...341).order("created_at DESC").limit(10)
     @electro_items = Product.where(category_id: 889...975).order("created_at DESC").limit(10)
     @toy_items = Product.where(category_id: 676...789).order("created_at DESC").limit(10)
+    @chanel_brands = Product.where(brand_id: 2447).order("created_at DESC").limit(10)
 
     @categories = Category.roots
     @products = @categories.map{|root| Product.where(category_id: root.subtree)}
