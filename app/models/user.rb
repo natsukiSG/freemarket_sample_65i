e1b05cb1501e2bb27dbfbb24807b92cf382643bc
yutawanme <yarakashiwanmen@gmail.com>
2020-01-11 21:20:24 +0900

    edit space

--- app/models/user.rb
+++ app/models/user.rb
@@ -91,7 +91,7 @@ class User < ApplicationRecord
 
 
   validates :nickname, presence: true,uniqueness: true, length: {maximum: 20}
-  validates :phone_number,presence: true,uniqueness: true
+  validates :phone_number, presence: true, uniqueness: true
 
   validates :first_name, :last_name, :first_name_kana, :last_name_kana,
   :birth_year,:birth_month,:birth_day, presence: true
