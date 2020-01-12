9f606d6dbb50d08f9f0c2544e6a6c46e8247d90d
lies046 <skytree.skysky@gmail.com>
2020-01-12 14:38:39 +0900

    productモデル修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id',optional: true
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id',optional: true
-  has_many :comments, dependent: :destroy
+  has_many :comments
   belongs_to :category,optional: true
 
   belongs_to :brand, optional: true
