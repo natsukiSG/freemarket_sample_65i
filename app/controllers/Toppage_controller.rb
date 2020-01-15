bad83d80b004eec9443e95b0b1cfcd3e7ffa2f47
lies046 <skytree.skysky@gmail.com>
2020-01-15 13:49:00 +0900

    修正

--- app/controllers/Toppage_controller.rb
+++ app/controllers/Toppage_controller.rb
@@ -35,16 +35,6 @@ class ToppageController < ApplicationController
     gon.count = 0
   end
 
-  def edit
-    @product = Product.find(params[:id])
-    gon.count = @product.images.length
-    if @product.buyer_id != nil || @product.seller_id != current_user.id
-      redirect_to root_path
-    end
-    @profit = (@product.price * 0.9).round
-    @fee = @product.price - @profit
-  end
-
   def show
     @product = Product.find(params[:id])
     @seller = @product.seller
